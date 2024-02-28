ccdecf368c6ea81c67376088230464435d2f664b
guilhermebastos2002 <gui.weil@hotmail.com>
2024-02-28 18:51:33 -0300

    novo

--- 4.go
+++ 4.go
@@ -2,45 +2,50 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 type Geometria struct {
-	B, H, L1, L2, op
+	B, H, L1, L2 float64
 }
 
 func (g Geometria) CalculaArea() float64 {
-	return (g.B*g.H)
+	return g.B * g.H
 }
 
 func (g Geometria) CalculaPerimetro() float64 {
-	return (g.L1*2+g.L2*2)
+	return 2*g.L1 + 2*g.L2
 }
 
 func main() {
-	fmt.Println("Qual operação deseja fazer?")
-	fmt.Println("Digite 1 para calcular a área, 2 para calcular o perímetro e 3 para sair")
-	for _, op := op {
+	var op int
+	var g Geometria
+
+	for {
+		fmt.Println("Qual operação deseja fazer?")
+		fmt.Println("Digite 1 para calcular a área, 2 para calcular o perímetro e 3 para sair")
+		fmt.Scan(&op)
+
 		switch op {
 		case 1:
 			fmt.Println("Digite a base do retângulo: ")
-			fmt.Scan(&B)
-			fmt.Println("Digite a altura do retângulo")
-			fmt.Scan(&H)
-			fmt.Println("A área desse retângulo é: ", CalculaArea())
-		
+			fmt.Scan(&g.B)
+			fmt.Println("Digite a altura do retângulo: ")
+			fmt.Scan(&g.H)
+			fmt.Println("A área desse retângulo é:", g.CalculaArea())
+
 		case 2:
 			fmt.Println("Digite o lado maior do retângulo: ")
-			fmt.Scan(&L1)
+			fmt.Scan(&g.L1)
 			fmt.Println("Digite o lado menor do retângulo: ")
-			fmt.Scan(&L2)
-			fmt.Println("O perímetro desse retângulo é: ", CalculaPerimetro())
+			fmt.Scan(&g.L2)
+			fmt.Println("O perímetro desse retângulo é:", g.CalculaPerimetro())
+
 		case 3:
 			fmt.Println("Saindo do programa...")
-			return 
+			return
+
 		default:
 			fmt.Println("Digite um valor válido.")
 		}
 	}
 }
-
